9926dabe5e73d76e732e8ddf80bfa1ccade9d8da
gonnavis <gonnavis@163.com>
2023-08-23 01:46:08 +0800

    Fix `events` bug.

--- scripts/mafia_game.js
+++ scripts/mafia_game.js
@@ -18,6 +18,8 @@ window.players = players;
 //   { "name": "Sebastian", "role": 'Civilian' }
 // ]
 
+const prompts = [];
+window.prompts = prompts;
 const events = [];
 window.events = events;
 const responseObjs = [];
@@ -44,7 +46,7 @@ Now is the first round of the game.
 
 ###
 Here are the events and conversations so far:
-${events.map((event) => event.content).join('\n')}
+${events.join('\n')}
 
 ###
 According to the rules of the game, you must choose one and only one other player that you think is a member of the Mafia, and explain why in short sentences.
@@ -61,24 +63,30 @@ Reply in this JSON format:
 `
     }]
   }
-  console.log('--- context: ', context);
+  console.log('--- prompt: ', context.messages[0].content);
+  prompts.push(context.messages[0].content);
+  // console.log('--- context: ', context);
 
-  console.log('------ _handleCreateQuestionSkill prompt before await:', context.messages)
+  // console.log('--- _handleCreateQuestionSkill prompt before await:', context.messages)
   const model = window.models.CreateModel('mafia_game:GPT 3.5 Turbo')
   context.messages = context.messages.map(message => JSON.stringify(message))
   window.models.ApplyContextObject(model, context);
-  console.log('--- payload module: ', window.models.GetModelWithContext(model))
+  // console.log('--- payload module: ', window.models.GetModelWithContext(model))
   const response = await window.models.CallModel(model);
-  console.log('------ _handleCreateQuestionSkill prompt:', context.messages)
-  console.log('------ _handleCreateQuestionSkill response:', response)
+  // console.log('--- _handleCreateQuestionSkill prompt:', context.messages)
+  // console.log('--- _handleCreateQuestionSkill response:', response)
   const responseObj = JSON.parse(response.choices[0].message.content)
-  console.log('------ _handleCreateQuestionSkill response JSON obj:', responseObj)
+  console.log('--- responseObj:', responseObj)
   responseObjs.push(responseObj);
 
   return responseObj
 }
 
 async function _handleStartGameSkill(event) {
+
+  prompts.length = 0;
+  events.length = 0;
+  responseObjs.length = 0;
 
   players.forEach((player) => {
     player.role = 'Civilian';
@@ -89,7 +97,7 @@ async function _handleStartGameSkill(event) {
     player.role = 'Mafia';
   })
   players.sort(() => 0.5 - Math.random());
-  console.log('--- players: ', players);
+  // console.log('--- players: ', players);
 
   for (let i = 0; i < players.length; i++) {
     const player = players[i];
@@ -98,10 +106,11 @@ async function _handleStartGameSkill(event) {
     events.push(`- ${player.name} accused ${responseObj.accuse} of being a Mafia.`);
   }
 
-  console.log('--- turn ended')
-  console.log('--- players: ', players);
-  console.log('--- events: ', events);
-  console.log('--- responseObjs: ', responseObjs);
+  console.log('------ turn ended')
+  console.log('------ players: ', players);
+  console.log('------ prompts: ', prompts);
+  console.log('------ events: ', events);
+  console.log('------ responseObjs: ', responseObjs);
 
   const isStop = true;
   return isStop;
